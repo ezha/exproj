3006eb13d66678690ad61437dcf492daab72d058
ezha <ezha@cowi.se>
2013-08-16 21:28:57 +0200

    Working version GOF Improve decluster and threshold POT

--- GodFit.R
+++ GodFit.R
@@ -6,7 +6,47 @@ library(MASS)
 library(lmom)
 library(VGAM)
 library(plyr)
-source("functions/cdflnorm.R")
+library(xts)
+
+# Create monthly time series longer than 10 years -----------------------------
+
+rLDfYmin <- ldply(rLDfY, summarise, min= min(m_o_h))
+
+rXts10Y <- vector("list", length(rXtsM))
+names(rXts10Y) <- names(rXts)
+Df10Y <- vector("list", length(rXtsM))
+
+for(i in 1:length(rXtsM)){
+  if(length(rXtsM[[i]]) >= 120){
+    rXts10Y[[i]] <- apply.yearly(lag(rXtsM[[i]],3),max)
+    #rXts10Y[[i]] <- rXts10Y[[i]][!is.na(rXts10Y[[i]])] 
+    Df10Y[[i]] <- data.frame(tid = index(rXts10Y[[i]]), 
+                              m_o_h = rXts10Y[[i]]$m_o_h, 
+                              Omr_stn = names(rXts[i]))
+    # get rid of NA values
+    
+    
+  }
+}
+
+rXts10Y <- rXts10Y[rXts10Y != "NULL"]
+Df10Y <- rDf10Y[Df10Y != "NULL"]
+
+# turn yearly xts to data.frame for further manipulation
+Df10Y <- lapply(Df10Y, function(x) 
+  ddply(x, .(Omr_stn), transform, 
+        norm = (m_o_h - mean(m_o_h, na.rm=T))/sd(m_o_h, na.rm=T)))
+
+
+# Choose length of series -----------------------------------------------------
+# at least 40 years:
+Df40Y
+# at least 10 years:
+Df10Y
+
+rLDfY <- Df10Y
+
+# Prepare GOF and fitting of series -------------------------------------------
 
 parGEV <- vector("list",length(rLDfY))
 adGEV <- vector("list",length(rLDfY))
@@ -30,6 +70,8 @@ ksLP3 <- vector("list",length(rLDfY))
 
 Lmom <- vector("list", length(rLDfY))
 
+# Estimate parameters and L-moments -------------------------------------------
+
 for(l in 1:length(rLDfY)){
   parNor[[l]] <- pelnor(samlmu(rLDfY[[l]]$m_o_h))
   parGEV[[l]] <- pelgev(samlmu(rLDfY[[l]]$m_o_h))
@@ -41,10 +83,12 @@ for(l in 1:length(rLDfY)){
                           error=function(e) e,finally = c(0, 1, 1))
   Lmom[[l]] <- data.frame(lskew = samlmu(rLDfY[[l]]$m_o_h)[[3]],
                           lkurt = samlmu(rLDfY[[l]]$m_o_h)[[4]],
-                          rör = names(rLDfY[l]),
-                          obs = "obs")
+                          Omr_stn = names(rLDfY[l]),
+                          obs = "obs",
+                          stlk = factor(2))
 }
 Lmom <- rbind.fill(Lmom)
+
 
 # Normal parameters -----------------------------------------------------------
 mean <- sapply(parNor, "[[", "mu")
@@ -70,7 +114,6 @@ delta <- tryCatch(sapply(parWei, "[[", "delta"), error=function(e) NULL)
 mu <- tryCatch(sapply(parLP3, "[[", "mu"), error=function(e) NULL)
 sigma <- tryCatch(sapply(parLP3, "[[", "sigma"), error=function(e) NULL)
 gamma <- tryCatch(sapply(parLP3, "[[", "gamma"), error=function(e) NULL)
-
 
 # GOF tests -------------------------------------------------------------------
 for(l in 1:length(rLDfY)){
@@ -113,22 +156,22 @@ for(l in 1:length(rLDfY)){
 
 # Anderson Darling
 ADNor <- data.frame(AD = sapply(adNor, "[[", "statistic"), FIT = "Normal")
-ADNor$rör <- names(rLDfY)
+ADNor$Omr_stn <- names(rLDfY)
 
 # Kolmogorov
 KSNor <- data.frame(KS = sapply(ksNor, "[[", "statistic"), FIT = "Normal")
-KSNor$rör <- names(rLDfY)
+KSNor$Omr_stn <- names(rLDfY)
 
 
 # prepare GOF results for data frame GEV --------------------------------------
 
 # Anderson Darling
 ADGEV <- data.frame(AD = sapply(adGEV, "[[", "statistic"), FIT = "GEV")
-ADGEV$rör <- names(rLDfY)
+ADGEV$Omr_stn <- names(rLDfY)
 
 # Kolmogorov
 KSGEV <- data.frame(KS = sapply(ksGEV, "[[", "statistic"), FIT = "GEV")
-KSGEV$rör <- names(rLDfY)
+KSGEV$Omr_stn <- names(rLDfY)
 
 
 # prepare GOF results for data frame Weibull ----------------------------------
@@ -137,13 +180,13 @@ KSGEV$rör <- names(rLDfY)
 names(adWei) <- names(rLDfY)
 adWei <- adWei[adWei != "1"]
 ADWei <- data.frame(AD = sapply(adWei, "[[", "statistic"),FIT = "Weibull")
-ADWei$rör <- names(adWei)
+ADWei$Omr_stn <- names(adWei)
 
 # Kolmogorov
 names(ksWei) <- names(rLDfY)
 ksWei <- ksWei[ksWei != "1"]
 KSWei <- data.frame(KS = sapply(ksWei, "[[", "statistic"),FIT = "Weibull")
-KSWei$rör <- names(ksWei)
+KSWei$Omr_stn <- names(ksWei)
 
 
 # prepare GOF results for data frame EV1 --------------------------------------
@@ -152,13 +195,13 @@ KSWei$rör <- names(ksWei)
 names(adEV1) <- names(rLDfY)
 adEV1 <- adEV1[adEV1 != "1"]
 ADEV1 <- data.frame(AD = sapply(adEV1, "[[", "statistic"),FIT = "EV1")
-ADEV1$rör <- names(adEV1)
+ADEV1$Omr_stn <- names(adEV1)
 
 # Kolmogorov
 names(ksEV1) <- names(rLDfY)
 ksEV1 <- ksEV1[ksEV1 != "1"]
 KSEV1 <- data.frame(KS = sapply(ksEV1, "[[", "statistic"),FIT = "EV1")
-KSEV1$rör <- names(ksEV1)
+KSEV1$Omr_stn <- names(ksEV1)
 
 
 # prepare GOF results for data frame LP3 --------------------------------------
@@ -167,13 +210,13 @@ KSEV1$rör <- names(ksEV1)
 names(adLP3) <- names(rLDfY)
 adLP3 <- adLP3[adLP3 != "1"]
 ADLP3 <- data.frame(AD = sapply(adLP3, "[[", "statistic"),FIT = "LP3")
-ADLP3$rör <- names(adLP3)
+ADLP3$Omr_stn <- names(adLP3)
 
 # Kolmogorov
 names(ksLP3) <- names(rLDfY)
 ksLP3 <- ksLP3[ksLP3 != "1"]
 KSLP3 <- data.frame(KS = sapply(ksLP3, "[[", "statistic"),FIT = "LP3")
-KSLP3$rör <- names(ksLP3)
+KSLP3$Omr_stn <- names(ksLP3)
 
 
 # Data frame with all AD-GOF results --> plotting -----------------------------
@@ -193,6 +236,16 @@ ggplot(ADFits, aes(FIT,AD)) +
   geom_text(stat="bin", color="black", vjust=1,
             aes(y=2.5, label=..count..))
 
+# Best fit AD ----------------------------------------------------------------
+ADBFex <- ddply(ADFits, .(Omr_stn), summarise, AD = min(AD))
+ADBF <- join(ADBFex, ADFits, by = c("Omr_stn","AD"), type = "inner")
+
+ggplot(ADBF, aes(FIT)) +
+  geom_bar(stat="bin") +
+  xlab("fördelning") + ylab("antal") +
+  theme_minimal()
+
+
 # Data frame with all KS-GOF results --> plotting -----------------------------
 
 KSNorok <- KSNor[which(KSNor$KS <= 0.12555),]
@@ -210,6 +263,14 @@ ggplot(KSFits, aes(FIT,KS)) +
   geom_text(stat="bin", color="black", vjust=1,
             aes(y=0.13, label=..count..))
 
+# Best fit KS ----------------------------------------------------------------
+KSBFex <- ddply(KSFits, .(Omr_stn), summarise, KS = min(KS))
+KSBF <- join(KSBFex, KSFits, by = c("Omr_stn","KS"), type = "inner")
+
+ggplot(KSBF, aes(FIT)) +
+  geom_bar(stat="bin") +
+  xlab("fördelning") + ylab("antal") +
+  theme_minimal()
 
 
 # plot to look at cdf vs ecdf -------------------------------------------------
@@ -227,29 +288,50 @@ ggplot(data=ggg, aes(x=val, y=CDF)) +
 
 # create necessary data:
 
-tau3 = seq(0, 0.6, length.out= 400)
+tau3 = seq(-0.1, 0.6, length.out= 400)
 
-EV1Lmom <- data.frame(dis = "EV1", lskew = 0.1699, lkurt = 0.1504)
-NorLmom <- data.frame(dis = "Normal", lskew = 0, lkurt = 0.1226)
+EV1Lmom <- data.frame(par2 = "EV1", lskew = 0.1699, lkurt = 0.1504)
+NorLmom <- data.frame(par2 = "Normal", lskew = 0, lkurt = 0.1226)
+WeiLmom <- data.frame(par3 = "Weibull", 
+                      lskew = (sapply(seq(-0.1,0.6, length.out = 400), 
+                        function(t3) lmrwei(pelwei(c(0,1,t3)), nmom=4)[[3]])),
+                      lkurt = (sapply(seq(-0.1,0.6, length.out = 400),                                           
+                        function(t3) lmrwei(pelwei(c(0,1,t3)), nmom=4)[[4]])))
 
-LP3Lmom <- data.frame(dist = rep("LP3",length(tau3)),
+LP3Lmom <- data.frame(par3 = rep("LP3",length(tau3)),
                       lskew = tau3, 
                       lkurt = 0.1224 + 0.30115 * tau3^2 + 0.95812 * tau3^4 + 
                               0.57488 * tau3^6 +  0.19383 * tau3^8)
 
-GEVLmom <- data.frame(dist = rep("GEV",length(tau3)),
+GEVLmom <- data.frame(par3 = rep("GEV",length(tau3)),
                       lskew = tau3, 
                       lkurt = 0.10701 + 0.1109 * tau3 + 0.84838 * tau3^2 + 
                         0.06669 * tau3^3 + 0.00567 * tau3^4 + 
                         0.04208 * tau3^5 + 0.03673 * tau3^6)
-L3par <- rbind.fill(LP3Lmom,GEVLmom)
+L3par <- rbind.fill(LP3Lmom,GEVLmom, WeiLmom)
 L2par <- rbind.fill(NorLmom,EV1Lmom)
 
+# Sample average
+LmomSampAvg <- data.frame(lskew = mean(Lmom$lskew),
+                          lkurt = mean(Lmom$lkurt),
+                          Omr_stn = factor("00_00"),
+                          obs = "Medelvärde",
+                          stlk = factor(5))
+Lmom10 <- rbind.fill(Lmom, LmomSampAvg)
+# Lmom40 <- Lmom
+
+# plot l-moment diagram with ggplot
+
 ggplot(data = L3par, aes(x=lskew, y=lkurt)) +
-  geom_line(aes(colour = dist)) +
-  geom_point(data = L2par, aes(colour= dis), size = 4, shape = 15) + 
-  geom_point(data = Lmom, aes(fill = obs)) +
+  geom_line(aes(colour = par3), size = 1.2) +
+  geom_point(data = L2par, aes(colour= par2), size = 5, shape = 15) + 
+  geom_point(data = Lmom, aes(fill = obs, shape = obs, size=stlk)) +
+  scale_size_manual(values=c(2,4), guide=FALSE) +
+#   geom_point(data = LmomSampAvg, aes(colour = dis), size = 5, shape = 12) +
   xlab(expression(italic(L) * "-skewness")) +
   ylab(expression(italic(L) * "-kurtosis")) +
-#   geom_text(data=Lmom, aes(label = rör, y = lkurt + 0.02, size=0.3)) +
-  ylim(0,0.4) + xlim(0,0.6)
+#   geom_text(data=Lmom, aes(label = Omr_stn, y = lkurt + 0.02, size=0.3)) +
+  ylim(0,0.4) + xlim(-0.1,0.6)
+
+# TODO 
+Line of Best fit
--- POT.R
+++ POT.R
@@ -1,38 +1,62 @@
-library(xts)
 
-# Skräp ----------------------
-bl<- coredata(rXtsM[[33]])
-index((rXtsM[[33]]))
-#-----------------------------
 
-rLDfYmin <- ldply(rLDfY, summarise, min= min(m_o_h))
-
-rXts10Y <- vector("list", length(rXtsM))
-names(rXts10Y) <- names(rXts)
-rDf10Y <- vector("list", length(rXtsM))
-
-for(i in 1:length(rXtsM)){
-  if(length(rXtsM[[i]]) >= 120){
-    rXts10Y[[i]] <- apply.yearly(lag(rXtsM[[i]],3),max)
-    #rXts10Y[[i]] <- rXts10Y[[i]][!is.na(rXts10Y[[i]])] 
-    rDf10Y[[i]] <- data.frame(tid = index(rXts10Y[[i]]), 
-                            m_o_h = rXts10Y[[i]]$m_o_h, 
-                            Omr_stn = names(rXts[i]))
-    # get rid of NA values
-    
-    
-  }
-}
-
-rXts10Y <- rXts10Y[rXts10Y != "NULL"]
-rDf10Y <- rDf10Y[rDf10Y != "NULL"]
-
-# turn yearly xts to data.frame for further manipulation
-rDf10Y <- lapply(rDf10Y, function(x) 
-  ddply(x, .(Omr_stn), transform, 
-        norm = (m_o_h - mean(m_o_h, na.rm=T))/sd(m_o_h, na.rm=T)))
+# POT with all values
 
 # TODO
 # 1. Create POT from 143 wells with lowest of five maximas as 
 # lowest threshold + declustering
+library(POT)
+library(xts)
 
+r.xts <- vector("list", length(r))
+names(r.xts) <- names(r)
+
+# Preallocate list for the new monthly aggregated xts
+r.xts.m <- vector("list", length(r))
+rDfM <- vector("list", length(r))
+
+# Preallocate list for the new monthly aggregated regular ts
+r.ts.m <- vector("list", length(r))
+names(r.ts.m) <- names(r)
+
+# Produce monthly data from
+for(i in 1:length(r)){ 
+  # make list items to xts objects
+  r.xts[[i]] <- xts(r[[i]]$m_o_h,as.Date(r[[i]]$Datum))
+  colnames(r.xts[[i]]) <- 'm_o_h'
+  
+  # aggregated monthly values  
+  r.xts.m[[i]] <- apply.monthly(r.xts[[i]],max)
+  
+  # transform to data.frame that can be used for further analysis and plotting
+  rDfM[[i]] <- data.frame(tid = index(r.xts.m[[i]]), 
+                          m_o_h = r.xts.m[[i]]$m_o_h, 
+                          Omr_stn = names(r[i]))
+}
+
+plot(rDfM[[2]]$m_o_h)
+x <- rDfM[[6]]$m_o_h
+xdf <- data.frame(time=rDfM[[6]]$tid,
+                  obs=rDfM[[6]]$m_o_h)
+
+tcplot(x, u.range = c(22.8, 23.2))
+
+u = 23.05
+
+mrlplot(x, u.range = c(22.6, 23.3))
+
+u = 23.05
+
+events <- clust(xdf, u = 23.05, tim.cond = 6, clust.max = TRUE, plot=TRUE)
+diplot(events, u.range = c(22, 23.5))
+
+npy = length(events[,"obs"]) / (diff(range(as.numeric(xdf$time), na.rm = TRUE))/356.25)
+
+plot(fitgpd(events[,"obs"], 23.05, "mle"), npy=npy)
+
+fitr6 <- fitgpd(events[,"obs"], 23.05, "mle")
+
+clust(data, 4, 12, clust.max = TRUE)
+flows <- obs
+date <- time
+diplot
